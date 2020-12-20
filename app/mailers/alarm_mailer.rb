cb97f873e970803f7e2920517ae7cfe2a83945b0
saeyama <camper_green_yellow_21@yahoo.co.jp>
2020-12-20 10:32:29 +0900

    whenever対応完了

--- app/mailers/alarm_mailer.rb
+++ app/mailers/alarm_mailer.rb
@@ -1,42 +1,154 @@
 class AlarmMailer < ApplicationMailer
   default from: 'noreply@example.com'
 
-  def alarm_mail
+  # def alarm_mail
+  #   users_notices_alarms = User.all.select do |user|
+  #     user.participants.where(notice: 'mail', alarm: "11:00" ).present?
+  #   end
+
+  #   users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+  #   mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  # end
+
+  #8:00am
+  def alarm_mail_at8
     users_notices_alarms = User.all.select do |user|
-      user.participants.where(notice: 'mail', alarm: "11:00" )
+      user.participants.where(notice: 'mail', alarm: "8:00" ).present?
     end
 
     users_notices_alarms_mails = users_notices_alarms.pluck(:email)
-
     mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
   end
 
+  #9:00am
+  def alarm_mail_at9
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "9:00" ).present?
+    end
+
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+  #10:00am
+  def alarm_mail_at10
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "10:00" ).present?
+    end
+
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+
+  #11:00am
+  def alarm_mail_at11
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "11:00" ).present?
+    end
+
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+  #0:00pm
+  def alarm_mail_at12
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "12:00" ).present?
+    end
   
-  # def alarm_mail
-  #   @user = User.pluck(:email)
-  #   Participant.where(alarm: '9:00')
-  #   mail(
-  #     bcc:   @user,
-  #     subject: 'anchorlink通知'
-  #   )
-  # end
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
 
-  # def alarm_mail_at9
-  #   @user = User.pluck(:email)
-  #   Participant.where(alarm: '9:00')
-  #   mail(
-  #     to:   @user,
-  #     subject: 'anchorlink通知'
-  #   )
-  # end
+  #1:00pm
+  def alarm_mail_at13
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "13:00" ).present?
+    end
+  
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
 
-  # def alarm_mail_at10
-  #   @user = User.pluck(:email)
-  #   Participant.where(alarm: '10:00')
-  #   mail(
-  #     to:   @user,
-  #     subject: 'anchorlink通知'
-  #   )
-  # end
+  #2:00pm
+  def alarm_mail_at14
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "14:00" ).present?
+    end
+  
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+  #3:00pm
+  def alarm_mail_at15
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "15:00" ).present?
+    end
+  
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+  #4:00pm
+  def alarm_mail_at16
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "16:00" ).present?
+    end
+  
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+  #5:00pm
+  def alarm_mail_at17
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "17:00" ).present?
+    end
+  
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+  #6:00pm
+  def alarm_mail_at18
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "18:00" ).present?
+    end
+  
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+  #7:00pm
+  def alarm_mail_at19
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "19:00" ).present?
+    end
+  
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+  #8:00pm
+  def alarm_mail_at20
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "20:00" ).present?
+    end
+  
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
+
+  #9:00pm
+  def alarm_mail_at21
+    users_notices_alarms = User.all.select do |user|
+      user.participants.where(notice: 'mail', alarm: "21:00" ).present?
+    end
+  
+    users_notices_alarms_mails = users_notices_alarms.pluck(:email)
+    mail(subject: "anchorlinkからのお知らせです！", to: users_notices_alarms_mails)
+  end
 
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -25,10 +25,75 @@ set :environment, :development
 #   runner "AnotherModel.prune_old_records"
 # end
 
-every 1.days, at: '9:00 am' do
-# every 1.minutes do
-  runner 'AlarmMailer.alarm_mail.deliver_now'
+#8:00am
+every 1.days, at: '11:00 pm' do
+  runner 'AlarmMailer.alarm_mail_at8.deliver_now'
 end
 
+#9:00am
+every 1.days, at: '0:00 am' do
+  runner 'AlarmMailer.alarm_mail_at9.deliver_now'
+end
+
+#10:00am
+every 1.days, at: '1:00 am' do
+  runner 'AlarmMailer.alarm_mail_at10.deliver_now'
+end
+
+#11:00am
+every 1.days, at: '2:00 am' do
+  runner 'AlarmMailer.alarm_mail_at11.deliver_now'
+end
+
+#0:00pm
+every 1.days, at: '3:00 am' do
+  # every 1.minutes do
+  runner 'AlarmMailer.alarm_mail_at12.deliver_now'
+end
+
+#1:00pm
+every 1.days, at: '4:00 am' do
+  runner 'AlarmMailer.alarm_mail_at13.deliver_now'
+end
+
+#2:00pm
+every 1.days, at: '5:00 am' do
+  runner 'AlarmMailer.alarm_mail_at14.deliver_now'
+end
+
+#3:00pm
+every 1.days, at: '6:00 am' do
+  runner 'AlarmMailer.alarm_mail_at15.deliver_now'
+end
+
+#4:00pm
+every 1.days, at: '7:00 am' do
+  runner 'AlarmMailer.alarm_mail_at16.deliver_now'
+end
+
+#5:00pm
+every 1.days, at: '8:00 am' do
+  runner 'AlarmMailer.alarm_mail_at17.deliver_now'
+end
+
+#6:00pm
+every 1.days, at: '9:00 am' do
+  runner 'AlarmMailer.alarm_mail_at18.deliver_now'
+end
+
+#7:00pm
+every 1.days, at: '10:00 am' do
+  runner 'AlarmMailer.alarm_mail_at19.deliver_now'
+end
+
+#8:00pm
+every 1.days, at: '11:00 am' do
+  runner 'AlarmMailer.alarm_mail_at20.deliver_now'
+end
+
+#9:00pm
+every 1.days, at: '0:00 pm' do
+  runner 'AlarmMailer.alarm_mail_at21.deliver_now'
+end
 
 # Learn more: http://github.com/javan/whenever
